2899b878e96e18f528b1709af27a91c595bc0aa4
Joseph Wilson <jw59615@gmail.com>
2020-04-27 19:18:05 -0400

    FET:added fetch endpoint and displaying the fetch

--- app/models.py
+++ app/models.py
@@ -3,3 +3,13 @@ from flask_migrate import Migrate
 
 db = SQLAlchemy()
 migrate = Migrate()
+
+
+class Strains(db.Model):
+    index = db.Column(db.BigInteger, primary_key=True)
+    strain = db.Column(db.String(128), nullable=False)
+    type_ = db.Column(db.String(128))
+    rating = db.Column(db.Float, nullable=False)
+    effects = db.Column(db.String(128), nullable=False)
+    flavor = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.String(128), nullable=False)
--- app/routes/web_routes.py
+++ app/routes/web_routes.py
@@ -1,15 +1,20 @@
 # web_app/routes/web_routes.py
 
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template, redirect, request
+
+from app.models import db, Strains
 
 web_routes = Blueprint("web_routes", __name__)
 
 
-@web_routes.route('/products/fetch', methods=["GET"])
+@web_routes.route('/products/fetch')
 def get_cards():
-    return "fetch ph"
+
+    db_strains = Strains.query.all()
+    return render_template("dbg.html", strains=db_strains)
 
 
-@web_routes.route("/products/query")
-def ask_cards():
-    return "query ph"
+@web_routes.route("/products/query/<name>", methods=['POST'])
+def ask_cards(name):
+    # get the name and serch for it in the db
+    return f"{name} was passed as a post command to db"
